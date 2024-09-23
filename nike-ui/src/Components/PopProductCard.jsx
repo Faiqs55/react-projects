b19afab40f5b5e1f88a8bac5fd73e5ded40721d8
Faiq Sindhu <sindhufaiq555@gmail.com>
2024-09-23 17:30:41 +0500

    update

--- nike-ui/src/Components/PopProductCard.jsx
+++ nike-ui/src/Components/PopProductCard.jsx
@@ -11,7 +11,7 @@ const PopProductCard = ({imgURL, name, price}) => {
           <p className='font-montserrat text-xl leading-normal text-slate-gray'>(4.5)</p>
         </div>
         <h3 className='mt-2 text-2xl leading-normal font-semibold font-palanquin'>{name}</h3>
-        <p className='mt-2 text-2xl text-coral-red font-semibold font-montserrat'>{price}</p>
+        <p className='mt-2  text-coral-red font-semibold font-montserrat'>{price}</p>
         {/* <Button label="Buy Now" styles={'bg-coral-red text-white mt-4 rounded-full'} /> */}
     </div>
   )
